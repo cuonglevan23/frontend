b7a1c67c920742f38e8c41c11eb176a273c06372
cuonglevan23 <103936303+cuonglevan23@users.noreply.github.com>
2022-11-29 10:19:15 +0700

    code new

--- src/pages/CheckOut.js
+++ src/pages/CheckOut.js
@@ -11,66 +11,53 @@ const Checkout = () => {
                                 <div className="row">
                                     <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12 ">
                                         <div className="ps-checkout__billing">
-                                            <h3>Billing Detail</h3>
+                                            <h3>CHI TIẾT THANH TOÁN</h3>
                                             <div className="form-group form-group--inline">
-                                                <label>First Name<span>*</span>
+                                                <label>Họ<span>*</span>
                                                 </label>
                                                 <input className="form-control" type="text" />
                                             </div>
                                             <div className="form-group form-group--inline">
-                                                <label>Last Name<span>*</span>
+                                                <label>Tên<span>*</span>
                                                 </label>
                                                 <input className="form-control" type="text" />
                                             </div>
+
                                             <div className="form-group form-group--inline">
-                                                <label>Company Name<span>*</span>
-                                                </label>
-                                                <input className="form-control" type="text" />
-                                            </div>
-                                            <div className="form-group form-group--inline">
-                                                <label>Email Address<span>*</span>
+                                                <label>Email <span>*</span>
                                                 </label>
                                                 <input className="form-control" type="email" />
                                             </div>
+
                                             <div className="form-group form-group--inline">
-                                                <label>Company Name<span>*</span>
+                                                <label>Số điện thoại<span>*</span>
                                                 </label>
                                                 <input className="form-control" type="text" />
                                             </div>
                                             <div className="form-group form-group--inline">
-                                                <label>Phone<span>*</span>
+                                                <label>Địa chỉ<span>*</span>
                                                 </label>
                                                 <input className="form-control" type="text" />
                                             </div>
-                                            <div className="form-group form-group--inline">
-                                                <label>Address<span>*</span>
-                                                </label>
-                                                <input className="form-control" type="text" />
-                                            </div>
-                                            <div className="form-group">
-                                                <div className="ps-checkbox">
-                                                    <input className="form-control" type="checkbox" id="cb01" />
-                                                    <label htmlFor="cb01">Create an account?</label>
-                                                </div>
-                                            </div>
-                                            <h3 className="mt-40"> Addition information</h3>
+
+                                            <h3 className="mt-40"> THÔNG TIN BỔ SUNG</h3>
                                             <div className="form-group form-group--inline textarea">
-                                                <label>Order Notes</label>
-                                                <textarea className="form-control" rows={5} placeholder="Notes about your order, e.g. special notes for delivery." defaultValue={""} />
+                                                <label>Ghi chú</label>
+                                                <textarea className="form-control" rows={5} defaultValue={""} />
                                             </div>
                                         </div>
                                     </div>
                                     <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 ">
                                         <div className="ps-checkout__order">
                                             <header>
-                                                <h3>Your Order</h3>
+                                                <h3>ĐƠN HÀNG CỦA BẠN</h3>
                                             </header>
                                             <div className="content">
                                                 <table className="table ps-checkout__products">
                                                     <thead>
                                                         <tr>
-                                                            <th className="text-uppercase">Product</th>
-                                                            <th className="text-uppercase">Total</th>
+                                                            <th className="text-uppercase">Sản Phẩm</th>
+                                                            <th className="text-uppercase">Tổng</th>
                                                         </tr>
                                                     </thead>
                                                     <tbody>
@@ -79,23 +66,23 @@ const Checkout = () => {
                                                             <td>$300.00</td>
                                                         </tr>
                                                         <tr>
-                                                            <td>Card Subtitle</td>
+                                                            <td>Tổng giá trị</td>
                                                             <td>$300.00</td>
                                                         </tr>
                                                         <tr>
-                                                            <td>Order Total</td>
-                                                            <td>$300.00</td>
+                                                            <td>Điểm nhận được</td>
+                                                            <td>10.000 điểm</td>
                                                         </tr>
                                                     </tbody>
                                                 </table>
                                             </div>
                                             <footer>
-                                                <h3>Payment Method</h3>
+                                                <h3>Phương thức thanh toán</h3>
                                                 <div className="form-group cheque">
                                                     <div className="ps-radio">
                                                         <input className="form-control" type="radio" id="rdo01" name="payment" defaultChecked />
-                                                        <label htmlFor="rdo01">Cheque Payment</label>
-                                                        <p>Please send your cheque to Store Name, Store Street, Store Town, Store State / County, Store Postcode.</p>
+                                                        <label htmlFor="rdo01">Giao hàng tận nhà – Trả tiền trực tiếp</label>
+                                                        <p>Trả tiền mặt cho người giao hàng (thu tiền hộ COD)</p>
                                                     </div>
                                                 </div>
                                                 <div className="form-group paypal">
@@ -104,18 +91,15 @@ const Checkout = () => {
                                                         <label htmlFor="rdo02">Paypal</label>
                                                     </div>
                                                     <ul className="ps-payment-method">
-                                                        <li><a href="#"><img src="images/payment/1.png" alt /></a></li>
-                                                        <li><a href="#"><img src="images/payment/2.png" alt /></a></li>
-                                                        <li><a href="#"><img src="images/payment/3.png" alt /></a></li>
+                                                        <li><a href="#"><img src="./img/payment/1.png" alt /></a></li>
+                                                        <li><a href="#"><img src="./img/payment/2.png" alt /></a></li>
+                                                        <li><a href="#"><img src="./img/payment/3.png" alt /></a></li>
                                                     </ul>
-                                                    <button className="ps-btn ps-btn--fullwidth">Place Order<i className="ps-icon-next" /></button>
+                                                    <button className="ps-btn ps-btn--fullwidth">Đặt Hàng<i className="ps-icon-next" /></button>
                                                 </div>
                                             </footer>
                                         </div>
-                                        <div className="ps-shipping">
-                                            <h3>FREE SHIPPING</h3>
-                                            <p>YOUR ORDER QUALIFIES FOR FREE SHIPPING.<br /> <a href="#"> Singup </a> for free shipping on every order, every time.</p>
-                                        </div>
+
                                     </div>
                                 </div>
                             </form>
